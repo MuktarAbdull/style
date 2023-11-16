6610bbc3597da8011c9f88d9430283912b84b249
Muktar Abdullahi Ommar <144605491+MuktarAbdull@users.noreply.github.com>
2023-11-16 10:57:39 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,39 @@
+            //ADDING STYLE in Js
+// let div = document.querySelector("div");
+// div.classList.add("succes")
+// div.classList.remove("succes")
+
+// div.classList.add("cu")
+
+// const age = HerAge().getAge;
+
+// if (HerAge <=21){
+//     Mydesire = "She is Perfect"
+// }else if(HerAge >21 && 23){
+//     Mydesire= "She is getting"
+// }else(HerAge>=25)
+// Mydesire = "You're ove age";
+
+// console.log(Mydesire)
+
+// let divv = document.querySelector("div");
+// let p = document.querySelector("p");
+// divv.classList.add("succes");
+// p.style.color = 'black';
+
+//My attempt
+
+let main = document.querySelector("mukiip");
+let h3 = document.querySelector("h3");
+let h2 = document.querySelector("h2");
+let h4 = document.querySelector("h4");
+
+
+h3.classList.add("mukiip");
+h3.style.color = 'darkorange';
+h2.style.margin = '5%'
+h2.style.color = '#fff'
+h4.style.margin = '5%'
+h4.style.fontFamily = '500'
+h4.style.color = 'darkorange'
+
